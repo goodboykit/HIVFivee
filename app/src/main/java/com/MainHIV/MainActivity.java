76339fa5df1f3fc19c593edce2ce8ad755293388
goodboykit <santiagogoodboykit@gmail.com>
2024-07-06 06:45:49 +0800

    lol
    lol

--- app/src/main/java/com/MainHIV/MainActivity.java
+++ app/src/main/java/com/MainHIV/MainActivity.java
@@ -1,14 +1,24 @@
 package com.MainHIV;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.Window;
+
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
     }
 }
