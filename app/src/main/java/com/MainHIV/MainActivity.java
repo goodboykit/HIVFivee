7e6b63b01df5bc377ff63b703d63039a42c04815
Kurt Cantuba <kurtbcantuba24@gmail.com>
2024-07-06 05:03:47 +0800

    Latest

--- app/src/main/java/com/MainHIV/MainActivity.java
+++ app/src/main/java/com/MainHIV/MainActivity.java
@@ -2,13 +2,26 @@ package com.MainHIV;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.firstpage);
+        setContentView(R.layout.activity_signup);
+
+//        Button switchButton = findViewById(R.id.btnSignIn);
+//        switchButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, login.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
