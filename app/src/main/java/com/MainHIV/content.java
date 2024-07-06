9304fe0a2bbde0e21f2fe98c999a88b4e64d0a93
goodboykit <santiagogoodboykit@gmail.com>
2024-07-06 08:31:47 +0800

    ddw
    dww

--- app/src/main/java/com/MainHIV/content.java
+++ app/src/main/java/com/MainHIV/content.java
@@ -1,16 +1,15 @@
 package com.MainHIV;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.VideoView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class content extends AppCompatActivity {
 
@@ -20,6 +19,24 @@ public class content extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_content);
+
+        // Set up the VideoView
+        VideoView videoView = findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.vid;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        // Set up the back button
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(content.this, tracker.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     public void click(View view) {
